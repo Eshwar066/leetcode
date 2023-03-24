572846ac623d0d149a196791bc21ddf4bb1da76b
Eshwar Sai <121691549+Eshwar066@users.noreply.github.com>
2023-03-24 19:57:05 +0530

    Time: 3 ms (33.39%), Space: 45.2 MB (51.28%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -21,6 +21,7 @@ class Solution {                          //striver dp-10
 }
 */
 
+/*
 class Solution {                          //striver dp-10
     public int minPathSum(int[][] grid) { //tabulation
         int n=grid.length;
@@ -50,6 +51,41 @@ class Solution {                          //striver dp-10
         }
         
         return dp[n-1][m-1];
+    }
+}
+*/
+
+class Solution {                          //striver dp-10
+    public int minPathSum(int[][] grid) { //DP
+        int n=grid.length;
+        int m=grid[0].length;
+        int[] prev=new int[m];
+        for(int i=0;i<n;i++){
+            int[] temp=new int[m];
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0) temp[j]=grid[i][j];
+                else{
+                int up=grid[i][j];
+                if(i>0){
+                    up+=prev[j];
+                }else{
+                    up += (int)Math.pow(10,9);
+                }
+                 int left=grid[i][j];
+                if(j>0){
+                    left+=temp[j-1];
+                }else{
+                    left += (int)Math.pow(10,9);
+                }
+                
+                temp[j]=Math.min(up,left);
+                }
+            }
+            prev=temp;
+            
+        }
+        
+        return prev[m-1];
     }
 }
 
