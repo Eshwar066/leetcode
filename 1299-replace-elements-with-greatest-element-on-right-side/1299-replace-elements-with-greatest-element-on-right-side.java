cdacc6844c1bb19779a7627233a551fdba928d71
Eshwar Sai <121691549+Eshwar066@users.noreply.github.com>
2023-03-27 12:48:29 +0530

    Time: 1004 ms (16.26%), Space: 45.6 MB (22.43%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,15 +1,34 @@
+// class Solution {
+//     public int[] replaceElements(int[] arr) {
+   
+//          int max = -1;
+
+//         for (int i = arr.length - 1; i >= 0; i--) {
+//             int temp = arr[i];
+//             arr[i] = max;
+//             max = Math.max(temp, max);
+//         }
+
+//         return arr;
+        
+//     }
+// }
+
 class Solution {
     public int[] replaceElements(int[] arr) {
-   
-         int max = -1;
+        int[] ans = new int[arr.length];
 
-        for (int i = arr.length - 1; i >= 0; i--) {
-            int temp = arr[i];
-            arr[i] = max;
-            max = Math.max(temp, max);
+        for (int i = 0; i < ans.length - 1; i++) {
+		
+            int max = Integer.MIN_VALUE;
+            for (int j = i + 1; j < ans.length; j++) {
+                max = Math.max(max, arr[j]);
+            }
+			
+            ans[i] = max;
         }
-
-        return arr;
-        
+		
+        ans[arr.length - 1] = -1;
+        return ans;
     }
 }
