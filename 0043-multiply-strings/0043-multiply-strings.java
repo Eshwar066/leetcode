1033c7a01f69d3eaa08097a6103e1558e34a7860
Eshwar Sai <121691549+Eshwar066@users.noreply.github.com>
2023-04-01 19:36:33 +0530

    Time: 9 ms (42.67%), Space: 43 MB (19.05%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,36 +1,49 @@
+// class Solution {
+//     public String multiply(String num1, String num2) {
+//         int m=num1.length(),
+//             n=num2.length();
+//         //base case
+//         if(m==0 || n==0 || "0".equals(num1) || "0".equals(num2)) return "0";
+        
+//         if("1".equals(num1)) return num2;
+        
+//         if("1".equals(num2)) return num1;
+        
+        
+//         //result can be maximum of length M+N.
+//         int[] result=new int[m+n];
+//         for(int i=m-1;i>=0;i--){
+//             for(int j=n-1;j>=0;j--){
+//                 int product=(num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+//                 //Adding previous value in result array into the product.
+//                 product+=result[i+j+1];
+                
+//                 //Adding the new product into the result array
+//                 result[i+j+1]=product%10;
+//                 result[i+j]+=product/10;
+                
+//             }
+//         }
+//         StringBuilder sb=new StringBuilder();
+//         for(int r:result){
+//             if(sb.length()==0 && r==0){
+//                 continue;
+//             }
+//             sb.append(r);
+//         }
+//         return sb.toString();
+//     }
+// }
+
+import java.math.*;
+
 class Solution {
     public String multiply(String num1, String num2) {
-        int m=num1.length(),
-            n=num2.length();
-        //base case
-        if(m==0 || n==0 || "0".equals(num1) || "0".equals(num2)) return "0";
+       
+        BigInteger n= new BigInteger(num1);
+        BigInteger m= new BigInteger(num2);
+        n=n.multiply(m);
+        return n.toString();
         
-        if("1".equals(num1)) return num2;
-        
-        if("1".equals(num2)) return num1;
-        
-        
-        //result can be maximum of length M+N.
-        int[] result=new int[m+n];
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                int product=(num1.charAt(i)-'0')*(num2.charAt(j)-'0');
-                //Adding previous value in result array into the product.
-                product+=result[i+j+1];
-                
-                //Adding the new product into the result array
-                result[i+j+1]=product%10;
-                result[i+j]+=product/10;
-                
-            }
-        }
-        StringBuilder sb=new StringBuilder();
-        for(int r:result){
-            if(sb.length()==0 && r==0){
-                continue;
-            }
-            sb.append(r);
-        }
-        return sb.toString();
     }
 }
