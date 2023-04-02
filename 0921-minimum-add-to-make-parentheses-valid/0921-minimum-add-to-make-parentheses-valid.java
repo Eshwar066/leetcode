64b4a3e20dcca383477f97536394d0fd47d12120
Eshwar Sai <121691549+Eshwar066@users.noreply.github.com>
2023-04-02 22:57:43 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (68.21%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int c1=0,c2=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(') c1++;
+            else if(c1>0) c1--;
+            else c2++;
+        }
+        return c1+c2;
+    }
+}
