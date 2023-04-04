015f2fd051ebc9aac2c40cbd0f62c934a54df0bc
Eshwar Sai <121691549+Eshwar066@users.noreply.github.com>
2023-04-04 09:07:59 +0530

    Time: 8 ms (31.15%), Space: 49.2 MB (24.84%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,4 +1,5 @@
-class Solution {                               //dp-21 //recursion
+/*
+class Solution {                               //dp-21 //recursion-memoization
     public int change(int amount, int[] coins) {
         int T=amount;
         int n=coins.length;
@@ -22,3 +23,32 @@ class Solution {                               //dp-21 //recursion
         return dp[ind][T]= not_take + take;
     }
 }
+*/
+
+class Solution {                               //dp-21 //Tabulation
+    public int change(int amount, int[] coins) {
+        int T=amount;
+        int n=coins.length;
+        int[][] dp=new int[n][T+1];
+         
+        for(int i=0;i<=T;i++){
+            if(i%coins[0]==0)  dp[0][i]=1;
+        }
+        
+        for(int ind=1;ind<n;ind++){
+            for(int t=0;t<=T;t++){
+                int not_take=dp[ind-1][t];
+                int take=0;
+                if(coins[ind]<=t)
+                     take= dp[ind][t-coins[ind]];
+                
+                dp[ind][t]=not_take+take;
+            }
+        }
+        return dp[n-1][T];
+    }
+}
+
+
+    
+    
