7f00c739d4eb538013c164c9e78db80fd7ab2385
Eshwar Sai <121691549+Eshwar066@users.noreply.github.com>
2023-04-24 23:46:43 +0530

    Time: 3 ms (96.39%), Space: 40.6 MB (66.06%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,29 +1,48 @@
-class Solution {
+// class Solution {
+//     public int countSubstrings(String s) {
+//         if(s.length()==0) return 0;
+//         int n=s.length();
+//         boolean[][] dp=new boolean[n][n];
+//         int res=0;
+        
+//         //base case1: length=1 -->true
+//         for(int i=0;i<n;i++){
+//             dp[i][i]=true;
+//             res++;
+//         }
+        
+//         //base case2: length=2 -->char[i]==char[i+1];
+//          for(int i=0;i<n-1;i++){
+//             dp[i][i+1]=s.charAt(i)==s.charAt(i+1);
+//             if(dp[i][i+1])  res++;
+//         }
+        
+//         //general case: length>2 --> char[s]==cahr[e] && dp[i+1][j-1];
+//         for(int len=3;len<=n;len++){
+//             for(int i=0,j=i+len-1; j<n; i++,j++){
+//                 dp[i][j]=s.charAt(i)==s.charAt(j) && dp[i+1][j-1];
+//                 if(dp[i][j]) res++;
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+class Solution {   
     public int countSubstrings(String s) {
-        if(s.length()==0) return 0;
-        int n=s.length();
-        boolean[][] dp=new boolean[n][n];
-        int res=0;
-        
-        //base case1: length=1 -->true
-        for(int i=0;i<n;i++){
-            dp[i][i]=true;
-            res++;
+        int count = 0;
+        for(int i = 0; i < s.length(); i++) // at every possible center of a palindrome
+            count += expandPalindrome(s, i, i) + expandPalindrome(s, i, i+1); // expand palindromes whose center is at index i for odd, or i and i+1 for even
+        return count;
+    }
+    
+    private int expandPalindrome(String s, int l, int r) {
+        int count = 0;
+        while(0 <= l && r < s.length() && s.charAt(l) == s.charAt(r)) { // is a palindrome while valid indexes and equal characters on both sides
+            count++;
+            l--;
+            r++;
         }
-        
-        //base case2: length=2 -->char[i]==char[i+1];
-         for(int i=0;i<n-1;i++){
-            dp[i][i+1]=s.charAt(i)==s.charAt(i+1);
-            if(dp[i][i+1])  res++;
-        }
-        
-        //general case: length>2 --> char[s]==cahr[e] && dp[i+1][j-1];
-        for(int len=3;len<=n;len++){
-            for(int i=0,j=i+len-1; j<n; i++,j++){
-                dp[i][j]=s.charAt(i)==s.charAt(j) && dp[i+1][j-1];
-                if(dp[i][j]) res++;
-            }
-        }
-        return res;
+        return count;
     }
 }
