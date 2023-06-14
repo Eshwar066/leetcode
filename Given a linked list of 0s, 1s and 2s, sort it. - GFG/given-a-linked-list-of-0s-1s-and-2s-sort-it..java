49754fbb431cc88bf91f03e4e65ea6283a8cc945
Eshwar Sai <121691549+Eshwar066@users.noreply.github.com>
2023-06-14 23:32:10 +0530

    Added solution - LeetHub

---
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().segregate(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head)
+    {
+        // add your code here
+        List<Integer> arr = new ArrayList<>();
+
+        Node curr = head;
+        while (curr != null) {
+            arr.add(curr.data);
+            curr = curr.next;
+        }
+         Integer[] sortedArr = arr.toArray(new Integer[0]);
+        Arrays.sort(sortedArr);
+       
+        Node dummy = new Node(0);
+        curr = dummy;
+        for (int val : sortedArr) {
+            curr.next = new Node(val);
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
+
+
