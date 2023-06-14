98f80f71336de3c1b0203422dd61798d4331fc73
Eshwar Sai <121691549+Eshwar066@users.noreply.github.com>
2023-06-14 22:06:10 +0530

    Time: 0 ms (100.00%), Space: 55.5 MB (86.91%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,29 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    
-     public void rotate(int[] nums, int k) {
-        // Ensure k is within array bounds
-        k %= nums.length;
-        // Reverse entire array
-        reverse(nums, 0, nums.length - 1);
-        // Reverse first k elements
-        reverse(nums, 0, k - 1);
-        // Reverse remaining elements
-        reverse(nums, k, nums.length - 1);
-    }
-    
-        public static void reverse(int nums[], int start, int end){
-    // While start index is less than end index
-    while(start < end){
-        // Swap elements at start and end indices
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
-        // Increment start index and decrement end index
-        start++;
-        end--;
+    public ListNode rotateRight(ListNode head, int k) {
+        List<Integer> list = new ArrayList<>();
+        ListNode curr = head;
+        while (curr != null) {
+            list.add(curr.val);
+            curr = curr.next;
+        }
+
+        int size = list.size();
+        if (size == 0) {
+            return null;
+        }
+
+        // Perform rotation
+        k = k % size;
+        if (k == 0) {
+            return head;
+        }
+
+        List<Integer> rotatedList = new ArrayList<>();
+        for (int i = size - k; i < size; i++) {
+            rotatedList.add(list.get(i));
+        }
+        for (int i = 0; i < size - k; i++) {
+            rotatedList.add(list.get(i));
+        }
+
+        // Convert the ArrayList back to linked list
+        ListNode dummy = new ListNode(0);
+        curr = dummy;
+        for (int val : rotatedList) {
+            curr.next = new ListNode(val);
+            curr = curr.next;
+        }
+
+        return dummy.next;
     }
 }
 
-   
-    
-}
