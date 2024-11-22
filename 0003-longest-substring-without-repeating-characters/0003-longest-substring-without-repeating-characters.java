226175cd0858061804211f83681ddabb2e2b1dba
Eshwar Sai <121691549+Eshwar066@users.noreply.github.com>
2024-11-22 21:50:35 +0530

    Time: 2 ms (98.66%), Space: 42.9 MB (96.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,18 +1,43 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {         //two pointer approach
-        int a_pointer=0,b_pointer=0,max=0;
 
-        HashSet<Character> hashSet=new HashSet();           //using hashset
-        while(b_pointer<s.length()){
-            if(!hashSet.contains(s.charAt(b_pointer))){
-                hashSet.add(s.charAt(b_pointer));
-                b_pointer++;
-                max=Math.max(hashSet.size(),max);
-            }else{
-                hashSet.remove(s.charAt(a_pointer));
-                a_pointer++;
+    public int lengthOfLongestSubstring(String s) {        
+        int a = 0, b = 0; 
+        int max = 0; 
+        int l = s.length(); 
+
+       
+        while (b < l) {
+          
+            for (int i = a; i < b; i++) {
+                if (s.charAt(i) == s.charAt(b)) {
+                    a = i + 1;
+                    break;
+                }
             }
+
+            
+            max = Math.max(max, b - a + 1);
+
+            b++; 
         }
-        return max; 
+
+        return max;
     }
+    
+    // public int lengthOfLongestSubstring(String s) {         //two pointer approach
+    //     int a_pointer=0,b_pointer=0,max=0;
+
+    //     HashSet<Character> hashSet=new HashSet<>();           //using hashset
+    //     while(b_pointer<s.length()){
+    //         if(!hashSet.contains(s.charAt(b_pointer))){
+    //             hashSet.add(s.charAt(b_pointer));
+    //             b_pointer++;
+    //             max=Math.max(hashSet.size(),max);
+    //         }else{
+    //             hashSet.remove(s.charAt(a_pointer));
+    //             a_pointer++;
+    //         }
+    //     }
+    //     return max; 
+    // }
 }
