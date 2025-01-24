33d0f2834640ea94f4f6bea297cf54ff1c4fb2dd
Eshwar Sai <121691549+Eshwar066@users.noreply.github.com>
2025-01-25 01:53:12 +0530

    Time: 371 ms, Memory: 41.4 MB - LeetHub

---
+++ 9905-278-37-sudoku-solver/9905-278-37-sudoku-solver.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+            sudoku(board); 
+    }
+
+    public boolean sudoku(char[][] board){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[i].length;j++){
+                if(board[i][j]=='.' ){
+                  for(char c='1';c<='9';c++){
+                    if(isValid(board,i,j,c)){
+                        board[i][j]=c;
+                        if(sudoku(board)){
+                            return true;
+                        }
+                         board[i][j]='.';
+                    }
+                  };
+                  return false;
+                }
+            }
+        };
+        return true;
+           
+    }
+    public boolean isValid(char[][] board,int row,int col, char c){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==c) return false;
+            if(board[row][i]==c) return false;
+            if(board[3*(row/3)+i/3][3 * (col/3) + i%3]==c)return false;
+        }
+        return true;
+    }
+}
